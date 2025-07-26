ddf827b060a6a1a208a0578203b3d51185ebc7d4
Michael Isaac <mbuotidemisaac@gmail.com>
2025-07-26 18:25:39 -0500

    Simplify deployment to frontend-only to get live URL working

--- src/lib/amplify.ts
+++ src/lib/amplify.ts
@@ -1,6 +1,11 @@
 import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
 
-Amplify.configure(outputs);
+// Basic configuration for frontend-only deployment
+const amplifyConfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+};
+
+Amplify.configure(amplifyConfig);
 
 export default Amplify;
